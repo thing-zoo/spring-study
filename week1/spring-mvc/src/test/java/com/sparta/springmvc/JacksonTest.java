2f2dbf2c6c803a4b9a005221b1e00ee59f41a121
thing-zoo <cathy4025@gmail.com>
2023-06-24 14:06:56 +0900

    learn about Jackson's Object Mapper

---
+++ week1/spring-mvc/src/test/java/com/sparta/springmvc/JacksonTest.java
@@ -0,0 +1,31 @@
+package com.sparta.springmvc;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sparta.springmvc.response.Star;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class JacksonTest {
+    @Test
+    @DisplayName("Object To JSON : get Method 필요")
+    void test1() throws JsonProcessingException {
+        Star star = new Star("Robbie", 95);
+
+        ObjectMapper objectMapper = new ObjectMapper(); // Jackson 라이브러리의 ObjectMapper
+        String json = objectMapper.writeValueAsString(star); // serialize(직렬화): object to json
+
+        System.out.println("json = " + json);
+    }
+
+    @Test
+    @DisplayName("JSON To Object : 기본 생성자 & (get OR set) Method 필요")
+    void test2() throws JsonProcessingException {
+        String json = "{\"name\":\"Robbie\",\"age\":95}"; // JSON 형태의 String, 키값과 field 변수명이 정확하게 일치해야함
+
+        ObjectMapper objectMapper = new ObjectMapper(); // Jackson 라이브러리의 ObjectMapper
+
+        Star star = objectMapper.readValue(json, Star.class); // deserialize(역직렬화): json to object
+        System.out.println("star.getName() = " + star.getName());
+    }
+}
