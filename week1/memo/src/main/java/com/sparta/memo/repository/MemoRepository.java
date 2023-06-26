bd2ed574890e4cf6e4cc43039f4fdd519e5f34b3
thing-zoo <cathy4025@gmail.com>
2023-06-26 15:48:57 +0900

    learn about Query Method

--- week1/memo/src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ week1/memo/src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -3,6 +3,11 @@ package com.sparta.memo.repository;
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MemoRepository extends JpaRepository<Memo, Long> { // <Entitiy, Id>
+    List<Memo> findAllByOrderByModifiedAtDesc();
+    List<Memo> findAllByUsername(String username);
+
 
 }
