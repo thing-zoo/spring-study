aea3c2571e5787494639d60383aee0bdea078762
thing-zoo <cathy4025@gmail.com>
2023-06-27 10:00:10 +0900

    learn about how to do if there is same type of 2 Beans

---
+++ week2/spring-auth/src/main/java/com/sparta/springauth/food/Pizza.java
@@ -0,0 +1,12 @@
+package com.sparta.springauth.food;
+
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Pizza implements Food {
+    @Override
+    public void eat() {
+        System.out.println("피자를 먹습니다.");
+    }
+}
---
+++ week2/spring-auth/src/test/java/com/sparta/springauth/BeanTest.java
@@ -0,0 +1,23 @@
+package com.sparta.springauth;
+
+import com.sparta.springauth.food.Food;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class BeanTest {
+    @Autowired
+    @Qualifier("pizza")
+    Food food;
+
+    @Test
+    @DisplayName("Primary & Qualifier 우선순위 확인")
+    void test1() {
+        food.eat(); // Qualifier가 높음
+    }
+
+
+}
