5cdd10ccc6b5b1257ca3d93941c0c6314a861307
thing-zoo <cathy4025@gmail.com>
2023-06-30 16:56:16 +0900

    move: 폴더 이동

--- week3/spring-auth/src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
+++ week3/spring-auth/src/main/java/com/sparta/springauth/dto/SignupRequestDto.java
@@ -2,6 +2,7 @@ package com.sparta.springauth.dto;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,8 +13,8 @@ public class SignupRequestDto {
     private String username;
     @NotBlank
     private String password;
-    @Email
     @NotBlank
+    @Pattern(regexp = "^[a-zA-Z0-9_!#$%&'*+/=?`{|}~^.-]+@[a-zA-Z0-9.-]+$", message = "이메일 형식에 맞지 않습니다.")
     private String email;
     private boolean admin = false;
     private String adminToken = "";
