aed086c534369531ce6604a1afab110d732fc139
thing-zoo <cathy4025@gmail.com>
2023-07-01 13:18:52 +0900

    learn about RestTemplate postForEntity Request

--- week4/spring-resttemplate-client/src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
+++ week4/spring-resttemplate-client/src/main/java/com/sparta/springresttemplateclient/service/RestTemplateService.java
@@ -1,6 +1,7 @@
 package com.sparta.springresttemplateclient.service;
 
 import com.sparta.springresttemplateclient.dto.ItemDto;
+import com.sparta.springresttemplateclient.entity.User;
 import lombok.extern.slf4j.Slf4j;
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -28,7 +29,7 @@ public class RestTemplateService {
         URI uri = UriComponentsBuilder
                 .fromUriString("http://localhost:7070") // 서버입장의 서버
                 .path("/api/server/get-call-obj")
-                .queryParam("query", query)
+                .queryParam("query", query) // query param 방식
                 .encode()
                 .build()
                 .toUri();
@@ -60,8 +61,25 @@ public class RestTemplateService {
     }
 
     public ItemDto postCall(String query) {
-        return null;
+        // 요청 URL 만들기
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:7070")
+                .path("/api/server/post-call/{query}") // path variable 방식
+                .encode()
+                .build()
+                .expand(query)
+                .toUri();
+        log.info("uri = " + uri);
+
+        User user = new User("Robbie", "1234");
+
+        ResponseEntity<ItemDto> responseEntity = restTemplate.postForEntity(uri, user, ItemDto.class);
+
+        log.info("statusCode = " + responseEntity.getStatusCode());
+
+        return responseEntity.getBody();
     }
+
 
     public List<ItemDto> exchangeCall(String token) {
         return null;
