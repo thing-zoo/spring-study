8ed5744c7e71199d88ae2f7a2512e2778fb18290
thing-zoo <cathy4025@gmail.com>
2023-08-13 22:55:52 +0900

    Controller 테스트

---
+++ week4/my-select-shop/src/test/java/com/sparta/myselectshop/mvc/MockSpringSecurityFilter.java
@@ -0,0 +1,27 @@
+package com.sparta.myselectshop.mvc;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+
+import java.io.IOException;
+
+// Spring Security가 동작하면 테스트에 방해가 됨
+// 따라서 가짜 필터를 만들어 가짜 인증을 위해 사용
+public class MockSpringSecurityFilter implements Filter {
+    @Override
+    public void init(FilterConfig filterConfig) {}
+
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+        SecurityContextHolder.getContext()
+                .setAuthentication((Authentication) ((HttpServletRequest) req).getUserPrincipal());
+        chain.doFilter(req, res);
+    }
+
+    @Override
+    public void destroy() {
+        SecurityContextHolder.clearContext();
+    }
+}
